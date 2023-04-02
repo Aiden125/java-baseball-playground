f54f62a4e6242a120341671bcb18bbd85c24220f
HeeSeok <mungml1995@gmail.com>
2023-04-02 23:23:48 +0900

    230402

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,6 +1,9 @@
 package study;
 
+
 import org.junit.jupiter.api.Test;
+
+import java.util.StringTokenizer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -9,5 +12,25 @@ public class StringTest {
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    /*
+    * 1. contains를 통한 배열 검증 가능
+    * */
+    @Test
+    void q1() {
+        String aa = "1,2";
+        String[] a = aa.split(",");
+        assertThat(a).contains("1");
+        assertThat(a).contains("2");
+//        assertThat(a).contains("3");
+    }
+
+    @Test
+    void q1_2() {
+        String aa = "1,2";
+        String[] a = aa.split(",");
+        assertThat(a).containsExactly("1", "2");
+//        assertThat(a).contains("3");
     }
 }
